9ae87903682a0e5eac1613e75350a29ca8cf593d
richard153r <richard153r@gmail.com>
2025-08-01 15:21:12 +0800

    0801

---
+++ Django/ch02/mblog/mysite/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from mysite.models import Post
+from datetime import datetime
+
+# Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    # return HttpResponse("Hello, world!")
+    return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+    post = Post.objects.get(slug=slug)
+    now = datetime.now()
+    return render(request, 'showpost.html', locals())
+
+def mptt(request):
+    return render(request, 'mqtt-dashboard-temp-json.html', locals())
